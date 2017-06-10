d8c5bfd06cdd455dd9aec40ba38657e58e56dab8
Oskar Polak <oskarbank@gmail.com>
2017-06-10 15:30:12 +0200

    asd

---
+++ src/main/java/com/example/PortfolioLukaszKolacz/models/Project.java
@@ -0,0 +1,67 @@
+package com.example.PortfolioLukaszKolacz.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+/**
+ * Created by Lenovo on 10.06.2017.
+ */
+@Entity
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String description;
+    private String name;
+    private String client;
+    private Date date;
+
+
+
+    public Project() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
